29f798c973c9752150faad7c623cbfa3bcd6511d
tanulo <tanulo@barossgabor.local>
2021-12-15 11:42:46 +0100

    1.1v

--- App.js
+++ App.js
@@ -1,98 +1,29 @@
-import React from 'react';
-import {StyleSheet, FlatList, ActivityIndicator, Text, View, Image , TouchableOpacity } from 'react-native';
-
-export default class FetchExample extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state ={ isLoading: true}
-  }
-
-  szavazat=(szam)=>{
-    //alert(szam)
-    var bemenet={
-      bevitel1:szam
-    }
-
-  fetch("http://localhost:3000/szavazatfelvitel", {
-      method: "POST",
-      body: JSON.stringify(bemenet),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    }
-  
-  )
-  .then(x => x.text())
-  .then(y => alert(y));
-
-  }
-
-
-  componentDidMount(){
-    return fetch('192.168.1.117/film')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-
-
-
-  render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => 
-
-          <View >
-          <Text style={{color:"brown",fontSize:20,textAlign:"center",marginTop:15,marginBottom:5}}   >{item.film_cim} </Text>
-          <Image  source={{uri: 'http://localhost:3000/'+item.film_kep}} style={{width:300,height:300,marginLeft:"auto",marginRight:"auto"}} />  
-
-          <TouchableOpacity
-        style={styles.kekgomb}
-        onPress={async ()=>this.szavazat(item.film_id)}
-      >
-        <Text style={{color:"white",fontWeight:"bold",fontSize:15}}  >Erre szavazok</Text>
-      </TouchableOpacity>
-          </View>
-        
-        }
-
-        
-          keyExtractor={({film_id}, index) => film_id}
-        />
-      </View>
-    );
-  }
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Menu from './Menu';
+import Helyek from './Helyek';
+function Menulap({ navigation }) {
+  return (
+    <Menu/>
+  );
 }
 
-const styles = StyleSheet.create({
-  
-  kekgomb: {
-    alignItems: "center",
-    backgroundColor: "blue",
-    padding: 10,
-    width:300,
-    marginLeft:"auto",
-    marginRight:"auto",
-  }
-});
+function Helyeklap({ navigation }) {
+  return (
+   <Helyek/>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Helyek">
+        <Drawer.Screen name="Menü" component={Menulap} />
+        <Drawer.Screen name="Helyek" component={Helyeklap} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
